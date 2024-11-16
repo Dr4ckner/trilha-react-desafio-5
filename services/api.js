834bc1d3245e761bbbba9faf6c4014affe14dcca
dr4ckner <leonardoluvian@gmail.com>
2024-11-16 02:37:54 -0300

    api key escondida

--- services/api.js
+++ services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const api  = axios.create({
     baseURL: 'https://xkzswbysbenfhjdrolmb.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrenN3YnlzYmVuZmhqZHJvbG1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MjYzOTEsImV4cCI6MjA0NzMwMjM5MX0.AAoo2u0SPDraWVfAE4jvPLXuoz5Ke1hIbn6lFj8IBD4",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrenN3YnlzYmVuZmhqZHJvbG1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3MjYzOTEsImV4cCI6MjA0NzMwMjM5MX0.AAoo2u0SPDraWVfAE4jvPLXuoz5Ke1hIbn6lFj8IBD4"
+        apikey: "api-key",
+        authorization: "authorization"
     }
 });
